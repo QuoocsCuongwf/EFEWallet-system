981471d7a99baab3dde5072687e6f2620d7bbf8e
Le Quoc Cuong <lequoccuong2204@gmail.com>
2026-05-22 19:23:25 +0700

    feat(wallet): implement core transfer logic and idempotent aspect

--- WalletService/src/main/java/com/QuoocsCuongwf/EFEWallet/WalletService/payload/KafkaMessage/TransferMessage.java
+++ WalletService/src/main/java/com/QuoocsCuongwf/EFEWallet/WalletService/payload/KafkaMessage/TransferMessage.java
@@ -1,4 +1,12 @@
 package com.QuoocsCuongwf.EFEWallet.WalletService.payload.KafkaMessage;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class TransferMessage {
+    private String emailSend;
+    private String emailRecive;
+    private BigDecimal amount;
+    private LocalDateTime time;
+    private String description;
 }
