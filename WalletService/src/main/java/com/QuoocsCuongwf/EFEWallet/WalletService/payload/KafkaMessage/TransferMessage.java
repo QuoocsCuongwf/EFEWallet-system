58f8a8e99431e7f9faad5a7ac0dde40f5572f988
Le Quoc Cuong <lequoccuong2204@gmail.com>
2026-06-04 22:27:51 +0700

    feat(wallet): add transactionId to transfer event message

--- WalletService/src/main/java/com/QuoocsCuongwf/EFEWallet/WalletService/payload/KafkaMessage/TransferMessage.java
+++ WalletService/src/main/java/com/QuoocsCuongwf/EFEWallet/WalletService/payload/KafkaMessage/TransferMessage.java
@@ -7,12 +7,14 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @Builder
 public class TransferMessage {
+    private UUID transactionId;
     private String walletSend;
     private String walletRecive;
     private BigDecimal amount;
