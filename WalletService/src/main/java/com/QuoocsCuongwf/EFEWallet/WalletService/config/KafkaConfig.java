ea7bab83d33dcd9be4ce3bf5cf9e17fa7b1d2722
Le Quoc Cuong <lequoccuong2204@gmail.com>
2026-06-16 19:31:31 +0700

    chore(wallet config): add objectMapper to use datetime in kafka

--- WalletService/src/main/java/com/QuoocsCuongwf/EFEWallet/WalletService/config/KafkaConfig.java
+++ WalletService/src/main/java/com/QuoocsCuongwf/EFEWallet/WalletService/config/KafkaConfig.java
@@ -1,7 +1,5 @@
 package com.QuoocsCuongwf.EFEWallet.WalletService.config;
 
-
-
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -20,7 +21,10 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> config = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
 
+        JsonSerializer<Object> jsonSerializer = new JsonSerializer<>(objectMapper);
         config.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
                 "localhost:9092"
