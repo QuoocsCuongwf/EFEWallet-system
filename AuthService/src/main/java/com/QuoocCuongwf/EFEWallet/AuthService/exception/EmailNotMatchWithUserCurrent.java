07ad0ec135d78c332ebc100ecb79c2798d2af446
Le Quoc Cuong <lequoccuong2204@gmail.com>
2026-06-18 17:20:40 +0700

    chore(auth): add email not match user current

--- AuthService/src/main/java/com/QuoocCuongwf/EFEWallet/AuthService/exception/EmailNotMatchWithUserCurrent.java
+++ AuthService/src/main/java/com/QuoocCuongwf/EFEWallet/AuthService/exception/EmailNotMatchWithUserCurrent.java
@@ -1,7 +1,9 @@
 package com.QuoocCuongwf.EFEWallet.AuthService.exception;
 
+import java.util.UUID;
+
 public class EmailNotMatchWithUserCurrent extends RuntimeException {
-    public EmailNotMatchWithUserCurrent(String message) {
-        super(message);
+    public EmailNotMatchWithUserCurrent(UUID userId, String email) {
+        super(userId + " not use "+ email);
     }
 }
