b9667374a367d49f24ffa2acac05238d8346442a
Le Quoc Cuong <lequoccuong2204@gmail.com>
2026-07-01 17:46:40 +0700

    chore(auth): add annotation

--- AuthService/src/main/java/com/QuoocCuongwf/EFEWallet/AuthService/payload/request/OtpRequest.java
+++ AuthService/src/main/java/com/QuoocCuongwf/EFEWallet/AuthService/payload/request/OtpRequest.java
@@ -3,12 +3,14 @@ package com.QuoocCuongwf.EFEWallet.AuthService.payload.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class OtpRequest {
     private String identifier;
     private String action;
